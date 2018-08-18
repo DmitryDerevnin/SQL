835006e401af52797e9e89ffbab171c1044dc4f8
Dmitry_Derevnin <derevnin@live.ru>
2018-08-18 15:57:28 +0400

    first commit

---
+++ FillTables.sql
@@ -0,0 +1,16 @@
+insert into Administrators (idAdministrator, Name, LastName) values (1, 'Semyon', 'Petrov');
+insert into Administrators (idAdministrator, Name, LastName) values (2, 'Evgeniy', 'Petrosyan');
+
+insert into Faculty (idFaculty, Name, EnrolleesLimit, Administrator_id) values (1, 'Faculty of Biotechnology', 10, 2);
+insert into Faculty (idFaculty, Name, EnrolleesLimit, Administrator_id) values (2, 'Faculty of Media Technology', 5, 2);
+insert into Faculty (idFaculty, Name, EnrolleesLimit, Administrator_id) values (3, 'Faculty of Computer Science', 20, 2);
+insert into Faculty (idFaculty, Name, EnrolleesLimit, Administrator_id) values (4, 'Faculty of Engineering', 13, 1);
+insert into Faculty (idFaculty, Name, EnrolleesLimit, Administrator_id) values (5, 'Faculty of Chemistry', 18, 1);
+
+insert into Enrollees (idEnrollee, Name, LastName) values (1, 'Oleg', 'Efremov');
+insert into Enrollees (idEnrollee, Name, LastName) values (2, 'Valentin', 'Nekrasov');
+insert into Enrollees (idEnrollee, Name, LastName) values (3, 'Faina', 'Drozdova');
+insert into Enrollees (idEnrollee, Name, LastName) values (4, 'Liliya', 'Kabanova');
+insert into Enrollees (idEnrollee, Name, LastName) values (5, 'Aleksey', 'Sobolev');
+insert into Enrollees (idEnrollee, Name, LastName) values (6, 'Anatoly', 'Makarov');
+
